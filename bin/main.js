67af89a9ba7f490f21768af7220b23b543bb527b
Zachary Guard <zacharyguard@gmail.com>
2018-02-06 11:17:50 +0800

    Add ability to show downloads by asset type and in total

--- bin/main.js
+++ bin/main.js
@@ -3,20 +3,13 @@
 /* require modules, initialize consts */
 
 const request = require("request");
+const minimist = require("minimist");
 const package = require("../package.json");
 
-const USAGE_STRING = "Usage: ghrs <username>/<repo name>";
+const USAGE_STRING = "Usage: ghrs <username>/<repo name> [--by-type|--by-release]";
 const USER_AGENT = `gh-releases-stats v${package.version}`;
 
 const repoRegex = /\S+\/\S+/;
-
-const sumDownloadCounts = function(assets) {
-  var sum = 0;
-  for (let i = 0, l = assets.length; i < l; i++) {
-    sum += assets[i].download_count;
-  }
-  return sum;
-};
 
 const zpadString = function(string, n) {
   n = n || 4;
@@ -27,13 +20,57 @@ const zpadString = function(string, n) {
   return chars.join("");
 };
 
+const getFileExtension = function(filename) {
+  let filenameSplit = filename.split(".");
+  return filenameSplit[filenameSplit.length - 1];
+};
+
+const sumReleaseDownloadCounts = function(assets) {
+  var sum = 0;
+  for (let i = 0, l = assets.length; i < l; i++) {
+    sum += assets[i].download_count;
+  }
+  return sum;
+};
+
+const sumAssetTypeDownloadCounts = function(releases) {
+  var map = {};
+  for (let i = 0, l = releases.length; i < l; i++) {
+    for (let j = 0, m = releases[i].assets.length; j < m; j++) {
+      let asset = releases[i].assets[j];
+      let ext = getFileExtension(asset.name);
+      if (!map.hasOwnProperty(ext)) {
+        map[ext] = 0;
+      }
+      map[ext] += asset.download_count;
+    }
+  }
+  return map;
+};
+
+const sumTotalDownloadCount = function(releases) {
+  var sum = 0;
+  for (let i = 0, l = releases.length; i < l; i++) {
+    for (let j = 0, m = releases[i].assets.length; j < m; j++) {
+      sum += releases[i].assets[j].download_count;
+    }
+  }
+  return sum;
+};
+
 /* parse arguments */
 
-let repo = process.argv[2];
+let args = minimist(process.argv.slice(2));
+
+let repo = args._[0];
 
 /* moving parts */
 
-if (repo && repoRegex.test(repo)) {
+if (args["version"] || args["v"]) {
+  console.log(`gh-release-stats v${package.version}`);
+} else if (args["help"] || args["h"]) {
+  console.log("\n" + USAGE_STRING + "\n");
+} else if (repo && repoRegex.test(repo)) {
   request({
     url: `https://api.github.com/repos/${repo}/releases`,
     headers: {
@@ -42,14 +79,33 @@ if (repo && repoRegex.test(repo)) {
   }, (err, response, body) => {
     if (err) throw err;
     let releases = JSON.parse(body);
-    for (let i = 0, l = releases.length; i < l; i++) {
-      let totalDownloadCount = sumDownloadCounts(releases[i].assets);
-      let releaseNameStr = zpadString(releases[i].name, 16);
-      let totalDownloadCountStr = zpadString(totalDownloadCount.toString(), 5);
-      let isPrereleaseStr = releases[i].prerelease ? " (pre-release)" : "";
-      console.log(`${releaseNameStr} : ${totalDownloadCountStr}${isPrereleaseStr}`);
+    if (args["by-type"]) {
+      let map = sumAssetTypeDownloadCounts(releases);
+      let exts = Object.keys(map).sort(function(a, b) {
+        if (map[a] > map[b]) return -1;
+        if (map[a] < map[b]) return 1;
+        return 0;
+      });
+
+      console.log("");
+      for (let i = 0, l = exts.length; i < l; i++) {
+        console.log(`${zpadString(exts[i], 8)} : ${zpadString(map[exts[i]].toString(), 5)} downloads`);
+      }
+      console.log("");
+    } else if (args["by-release"]) {
+      console.log("");
+      for (let i = 0, l = releases.length; i < l; i++) {
+        let releaseNameStr = zpadString(releases[i].name, 16);
+        let totalDownloadCount = sumReleaseDownloadCounts(releases[i].assets);
+        let totalDownloadCountStr = zpadString(totalDownloadCount.toString(), 5);
+        let isPrereleaseStr = releases[i].prerelease ? " (pre-release)" : "";
+        console.log(`${releaseNameStr} : ${totalDownloadCountStr} downloads${isPrereleaseStr}`);
+      }
+      console.log("");
+    } else {
+      console.log(`\n${sumTotalDownloadCount(releases)} total downloads\n`);
     }
   });
 } else {
-  console.log("No valid repository name provided.\n" + USAGE_STRING);
+  console.log("\nNo valid repository name provided.\n" + USAGE_STRING + "\n");
 }
